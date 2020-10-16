a0bf2b4c8dc1f3fc9f4fb67098e9e57278f75f74
gmlevenhagen <garettlevenhagen@gmail.com>
2020-10-16 06:39:05 -0500

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+class MP3Importer
+
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        Dir.chdir(@path) do | path |
+            Dir.glob("*.mp3")
+        end
+      end
+
+    def import 
+        files.each {|song| Song.new_by_filename(song)}
+    end
+
+
+end
